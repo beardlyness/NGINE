beb0321c64bceadc432eaee179084871481eaa25
Beard <beardlyness@users.noreply.github.com>
2019-04-08 11:20:14 -0700

    V :0.0.5 Alpha
    # title            :NGINE.sh
    # description      :This script will make it super easy to setup a LEMP server with selected Addons.
    # author           :The Crypto World Foundation.
    # contributors     :beard
    # date             :04-08-2019
    # version          :0.0.5 Alpha
    # os               :Debian/Ubuntu
    # usage            :bash ngine.sh
    # notes            :If you have any problems feel free to email the maintainer: beard [AT] cryptoworld [DOT] is

--- ngine.sh
+++ ngine.sh
@@ -19,15 +19,15 @@
 # description      :This script will make it super easy to setup a LEMP server with selected Addons.
 # author           :The Crypto World Foundation.
 # contributors     :beard
-# date             :04-05-2019
-# version          :0.0.4 Alpha
+# date             :04-08-2019
+# version          :0.0.5 Alpha
 # os               :Debian/Ubuntu
 # usage            :bash ngine.sh
 # notes            :If you have any problems feel free to email the maintainer: beard [AT] cryptoworld [DOT] is
 #===============================================================================================================================================
 
 # Force check for root
-  if ! [ $(id -u) = 0 ]; then
+  if ! [ "$(id -u)" = 0 ]; then
     echo "You need to be logged in as root!"
       exit 1
   fi
@@ -42,15 +42,15 @@
 
   # Setting up different NGINX branches to prep for install
     function nginx_stable() {
-        echo deb http://nginx.org/packages/$system/ $flavor nginx > /etc/apt/sources.list.d/$flavor.nginx.stable.list
-        echo deb-src http://nginx.org/packages/$system/ $flavor nginx >> /etc/apt/sources.list.d/$flavor.nginx.stable.list
+        echo deb http://nginx.org/packages/"$system"/ "$flavor" nginx > /etc/apt/sources.list.d/"$flavor".nginx.stable.list
+        echo deb-src http://nginx.org/packages/"$system"/ "$flavor" nginx >> /etc/apt/sources.list.d/"$flavor".nginx.stable.list
           wget https://nginx.org/keys/nginx_signing.key
           apt-key add nginx_signing.key
       }
 
     function nginx_mainline() {
-        echo deb http://nginx.org/packages/mainline/$system/ $flavor nginx > /etc/apt/sources.list.d/$flavor.nginx.mainline.list
-        echo deb-src http://nginx.org/packages/mainline/$system/ $flavor nginx >> /etc/apt/sources.list.d/$flavor.nginx.mainline.list
+        echo deb http://nginx.org/packages/mainline/"$system"/ "$flavor" nginx > /etc/apt/sources.list.d/"$flavor".nginx.mainline.list
+        echo deb-src http://nginx.org/packages/mainline/"$system"/ "$flavor" nginx >> /etc/apt/sources.list.d/"$flavor".nginx.mainline.list
           wget https://nginx.org/keys/nginx_signing.key
           apt-key add nginx_signing.key
       }
@@ -67,19 +67,24 @@
         wget -O /etc/security/limits.conf https://raw.githubusercontent.com/beardlyness/NGINE/master/etc/security/limits.conf
       echo "Setting up background NGINX workers.."
         wget -O /etc/default/nginx https://raw.githubusercontent.com/beardlyness/NGINE/master/etc/default/nginx
-      echo "Setting up configuration file for NGINX.."
-        wget -O /etc/nginx/conf.d/nginx-website.conf https://raw.githubusercontent.com/beardlyness/NGINE/master/etc/conf.d/ssldev.conf
-      echo "Setting up folders.."
-        mkdir -p /etc/engine/ssl/live
-        mkdir -p /var/www/html/pub/live
 
+      # Attached grab for Domain Name
         read -r -p "Domain Name: (Leave { HTTPS:/// | HTTP:// | WWW. } out of the domain) " DOMAIN
-          if [[ "${DOMAIN,,}" ]]
+          if [[ -n "${DOMAIN,,}" ]]
             then
+              echo "Setting up configuration file for NGINX.."
+                wget -O /etc/nginx/conf.d/"$DOMAIN".conf https://raw.githubusercontent.com/beardlyness/NGINE/master/etc/conf.d/ssl-nginx-website.conf
               echo "Changing 'server_name foobar' >> server_name '$DOMAIN' .."
-                sed -i 's/server_name foobar/server_name '$DOMAIN'/g' /etc/nginx/conf.d/nginx-website.conf
-                sed -i 's/server_name www.foobar/server_name www.'$DOMAIN'/g' /etc/nginx/conf.d/nginx-website.conf
+                sed -i 's/server_name foobar/server_name '"$DOMAIN"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
+                sed -i 's/server_name www.foobar/server_name www.'"$DOMAIN"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
+              echo "Fixing up the site configuration file for NGINX.."
+                sed -i 's/domain/'"$DOMAIN"'/g' /etc/nginx/conf.d/"$DOMAIN".conf
               echo "Domain Name has been set to: '$DOMAIN' "
+              echo "Setting up folders.."
+                mkdir -p /etc/engine/ssl/"$DOMAIN"
+                mkdir -p /var/www/html/"$DOMAIN"/live
+            else
+              echo "Sorry we cannot live on! RIP Dead.."
           fi
     }
 
@@ -89,22 +94,22 @@
             wget -O -  https://raw.githubusercontent.com/Neilpang/acme.sh/master/acme.sh | INSTALLONLINE=1  sh
             reset
             service nginx stop
-            openssl dhparam -out /etc/engine/ssl/live/dhparam.pem 2048
-            bash ~/.acme.sh/acme.sh --issue --standalone -d $DOMAIN -d www.$DOMAIN -ak 4096 -k 4096 --force
-            bash ~/.acme.sh/acme.sh --install-cert -d $DOMAIN \
-              --key-file    /etc/engine/ssl/live/ssl.key \
-              --fullchain-file    /etc/engine/ssl/live/certificate.cert \
+            openssl dhparam -out /etc/engine/ssl/"$DOMAIN"/dhparam.pem 2048
+            bash ~/.acme.sh/acme.sh --issue --standalone -d "$DOMAIN" -d www."$DOMAIN" -ak 4096 -k 4096 --force
+            bash ~/.acme.sh/acme.sh --install-cert -d "$DOMAIN" \
+              --key-file    /etc/engine/ssl/"$DOMAIN"/ssl.key \
+              --fullchain-file    /etc/engine/ssl/"$DOMAIN"/certificate.cert \
               --reloadcmd   "service nginx restart"
     }
 
     #Prep for SSL setup for Qualys rating
     function sslqualy() {
       echo "Preparing to setup NGINX to meet Qualys 100% Standards.."
-        sed -i 's/ssl_prefer_server_ciphers/#ssl_prefer_server_ciphers/g' /etc/nginx/conf.d/nginx-website.conf
-        sed -i 's/#ssl_ciphers/ssl_ciphers/g' /etc/nginx/conf.d/nginx-website.conf
-        sed -i 's/#ssl_ecdh_curve/ssl_ecdh_curve/g' /etc/nginx/conf.d/nginx-website.conf
+        sed -i 's/ssl_prefer_server_ciphers/#ssl_prefer_server_ciphers/g' /etc/nginx/conf.d/"$DOMAIN".conf
+        sed -i 's/#ssl_ciphers/ssl_ciphers/g' /etc/nginx/conf.d/"$DOMAIN".conf
+        sed -i 's/#ssl_ecdh_curve/ssl_ecdh_curve/g' /etc/nginx/conf.d/"$DOMAIN".conf
       echo "Generating a 4096 DH Param. This may take a while.."
-        openssl dhparam -out /etc/engine/ssl/live/dhparam.pem 4096
+        openssl dhparam -out /etc/engine/ssl/"$DOMAIN"/dhparam.pem 4096
       echo "Restarting NGINX Service..."
         service nginx restart
         service nginx status
@@ -157,8 +162,8 @@
     fi
 
     # Grabbing info on active machine.
-        flavor=`lsb_release -cs`
-        system=`lsb_release -i | grep "Distributor ID:" | sed 's/Distributor ID://g' | sed 's/["]//g' | awk '{print tolower($1)}'`
+        flavor=$(lsb_release -cs)
+        system=$(lsb_release -i | grep "Distributor ID:" | sed 's/Distributor ID://g' | sed 's/["]//g' | awk '{print tolower($1)}')
 
 # NGINX Arg main
 read -r -p "Do you want to setup NGINX as a Web Server? (Y/N) " REPLY
@@ -246,11 +251,11 @@ read -r -p "Do you want to install and setup PHP? (Y/N) " REPLY
             sed -i 's/listen.owner = www-data/listen.owner = nginx/g' /etc/php/7.1/fpm/pool.d/www.conf
             sed -i 's/listen.group = www-data/listen.group = nginx/g' /etc/php/7.1/fpm/pool.d/www.conf
             sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.1/fpm/php.ini
-            sed -i 's/phpx.x-fpm.sock/php7.1-fpm.sock/g' /etc/nginx/conf.d/nginx-website.conf
+            sed -i 's/phpx.x-fpm.sock/php7.1-fpm.sock/g' /etc/nginx/conf.d/"$DOMAIN".conf
             service php7.1-fpm restart
             service php7.1-fpm status
             service nginx restart
-            ps aux | grep -v root | grep php-fpm | cut -d\  -f1 | sort | uniq
+            pgrep -v root | pgrep php-fpm | cut -d\  -f1 | sort | uniq
           ;;
       2)
         echo "Installing PHP 7.2, and its modules.."
@@ -260,11 +265,11 @@ read -r -p "Do you want to install and setup PHP? (Y/N) " REPLY
             sed -i 's/listen.owner = www-data/listen.owner = nginx/g' /etc/php/7.2/fpm/pool.d/www.conf
             sed -i 's/listen.group = www-data/listen.group = nginx/g' /etc/php/7.2/fpm/pool.d/www.conf
             sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.2/fpm/php.ini
-            sed -i 's/phpx.x-fpm.sock/php7.2-fpm.sock/g' /etc/nginx/conf.d/nginx-website.conf
+            sed -i 's/phpx.x-fpm.sock/php7.2-fpm.sock/g' /etc/nginx/conf.d/"$DOMAIN".conf
             service php7.2-fpm restart
             service php7.2-fpm status
             service nginx restart
-            ps aux | grep -v root | grep php-fpm | cut -d\  -f1 | sort | uniq
+            pgrep -v root | pgrep php-fpm | cut -d\  -f1 | sort | uniq
           ;;
       3)
         echo "Installing PHP 7.3, and its modules.."
@@ -274,11 +279,11 @@ read -r -p "Do you want to install and setup PHP? (Y/N) " REPLY
            sed -i 's/listen.owner = www-data/listen.owner = nginx/g' /etc/php/7.3/fpm/pool.d/www.conf
            sed -i 's/listen.group = www-data/listen.group = nginx/g' /etc/php/7.3/fpm/pool.d/www.conf
            sed -i 's/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g' /etc/php/7.3/fpm/php.ini
-           sed -i 's/phpx.x-fpm.sock/php7.3-fpm.sock/g' /etc/nginx/conf.d/nginx-website.conf
+           sed -i 's/phpx.x-fpm.sock/php7.3-fpm.sock/g' /etc/nginx/conf.d/"$DOMAIN".conf
            service php7.3-fpm restart
            service php7.3-fpm status
            service nginx restart
-           ps aux | grep -v root | grep php-fpm | cut -d\  -f1 | sort | uniq
+           pgrep -v root | pgrep php-fpm | cut -d\  -f1 | sort | uniq
           ;;
     esac
 clear
@@ -302,15 +307,15 @@ esac
               echo "Setting up PHPMyAdmin.."
                 apt-get install phpmyadmin
                 apt-get install libmcrypt-dev
-                ln -s /usr/share/phpmyadmin /var/www/html/pub/live
+                ln -s /usr/share/phpmyadmin /var/www/html/"$DOMAIN"/live
 
                 # Changes URL/phpmyadmin >> URL/Custom+String
                 read -r -p "Custom PHPMyAdmin URL String: " REPLY
                   if [[ "${REPLY,,}" =~ ^[a-zA-Z0-9_.-]*$ ]]
                     then
-                      echo "Changing /var/www/html/pub/live/phpmyadmin >> /var/www/html/pub/live/'$REPLY' "
-                        mv /var/www/html/pub/live/phpmyadmin /var/www/html/pub/live/$REPLY
-                      echo "You can now access PHPMyAdmin with Username: 'phpmyadmin' via: https://$DOMAIN/$REPLY"
+                      echo "Changing /var/www/html/""$DOMAIN""/live/phpmyadmin >> /var/www/html/""$DOMAIN""/live/""$REPLY"" "
+                        mv /var/www/html/"$DOMAIN"/live/phpmyadmin /var/www/html/"$DOMAIN"/live/"$REPLY"
+                      echo "You can now access PHPMyAdmin with Username: 'phpmyadmin' via: https://""$DOMAIN""/""$REPLY"" "
                     else
                       echo "Only Letters & Numbers are allowed."
                   fi
